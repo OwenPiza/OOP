7f1262beebea1ea09e04688d6447588d14fd93f7
OwenPiza <opizaa@udistrital.edu.co>
2025-04-08 17:29:13 -0500

    Add files via upload

---
+++ Corte 1/Tarea_1/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Operations resultado = new Operations();
+        resultado. sumar (3,2);
+        System.out.println(resultado.r);
+        
+        resultado.restar (5,4);
+        System.out.println (resultado.r);
+        
+        resultado.multiplicar(6,8);
+        System.out.println (resultado.r);
+        
+        resultado.potencia(4,6);
+        System.out.println(resultado.r);
+        
+        
+    }
+    
+}
